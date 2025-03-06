3235c9fc4fc6d043a08eec5f16acc12fecfef9a6
Markus <markus.vickman@hotmail.com>
2025-03-06 14:39:13 +0100

    Validerat html och css

--- obj/Debug/net9.0/discgolf-duels.AssemblyInfo.cs
+++ obj/Debug/net9.0/discgolf-duels.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("discgolf-duels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacf847fc487471a48a645e30fd14b2306ea6334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29fc0dbcf5984a156fe1f992af7918a0a286b380")]
 [assembly: System.Reflection.AssemblyProductAttribute("discgolf-duels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("discgolf-duels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
